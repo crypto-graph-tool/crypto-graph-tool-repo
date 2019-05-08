3e316454e5c2d5fd324756ef7fa079bb1b979473
Andrew Schuessler <ajschuessler@gmail.com>
2019-05-08 16:06:08 -0400

    Implemented chartjs chart using react-chartjs populated with placeholder data.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const LineChart = require("react-chartjs").Line;
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fillColor: "rgba(220,220,220,0.2)",
+                        strokeColor: "rgba(220,220,220,1)",
+                        pointColor: "rgba(220,220,220,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(220,220,220,1)",
+                        data: [65, 59, 80, 81, 56, 55, 40]
+                    },
+                    {
+                        label: "My Second dataset",
+                        fillColor: "rgba(151,187,205,0.2)",
+                        strokeColor: "rgba(151,187,205,1)",
+                        pointColor: "rgba(151,187,205,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(151,187,205,1)",
+                        data: [28, 48, 40, 19, 86, 27, 90]
+                    }
+                ]
+            }
+        }
     }
 
     render() {
         return (
-            <div>crypto app</div>
+            <div>
+
+                <LineChart data={this.state.data} width="600" height="250"/>
+
+            </div>
+            
         )
     }
 }
