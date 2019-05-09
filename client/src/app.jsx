641f915026c157804114af6068a3c38e01f0f820
Alan Zhao <alan.zhao908@gmail.com>
2019-05-09 12:14:42 -0400

    done

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -78,6 +78,7 @@ class App extends React.Component {
                         onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
                         focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                         onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                        isOutsideRange={() => false}
                     />
                 </div>
                 <div>
